3ac67bc49d65c49bb339e46a315565b242e11dc2
Ori Sne <orisne@greeneye.ag>
2024-12-12 15:21:38 +0200

    added a script to get clusters from fleet (rt-version)

---
+++ scripts/get-fleet-clusters.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+FLEET_PATH=$($SCRIPT_DIR/find-dir.sh --pattern "rt-versions")clusters
+CONFIGMAP_NAME="cluster-vars-cm.yaml"
+
+print_help() {
+    cat << EOF
+Usage: $(basename "$0") [OPTIONS]
+
+Options:
+  -p, --pattern    Search pattern for filtering clusters (default: 'office')
+  -h, --help       Display this help message
+EOF
+    exit 0
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -h|--help)
+            print_help
+            ;;
+        -p|--pattern)
+            PATTERN="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            print_help
+            ;;
+    esac
+done
+
+PATTERN=${PATTERN:-"office"}
+
+
+if ! [ -d $FLEET_PATH ]; then
+    echo "error: path not found $FLEET_PATH"
+fi
+
+cluster_cmd="awk -F': ' '/CLUSTER|SPRAYER_GROUP|BOOM_LOCATION/ {split(\$2, arr, \"#\"); printf \"%s \", arr[1]} END {print \"\"}' {}"
+
+if command -v "fd" 2>&1 >/dev/null; then
+    find_cmd="fd -t f $CONFIGMAP_NAME $FLEET_PATH --exec $cluster_cmd"
+else
+    find_cmd="find $FLEET_PATH -type f -name $CONFIGMAP_NAME -exec $cluster_cmd \;"
+fi
+
+if command -v "rg" 2>&1 >/dev/null; then
+    bash -c "$find_cmd" | rg "$PATTERN"
+else
+    bash -c "$find_cmd" | grep "$PATTERN"
+fi
